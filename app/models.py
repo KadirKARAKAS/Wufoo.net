edb23227766808fd40526315e6caa4cd82b1c270
Beytullah Soy <114873625+bytsyw@users.noreply.github.com>
2024-05-20 20:40:59 +0300

    modelde değişiklikler yapıldı

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ from app import db, login_manager
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+# models.py
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
@@ -14,6 +15,7 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(60), nullable=False)
     role = db.Column(db.String(10), nullable=False, default='user')
     posts = db.relationship('Post', backref='author', lazy=True)
+    comments = db.relationship('Comment', backref='user', lazy=True)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}', '{self.role}')"
@@ -25,6 +27,7 @@ class Post(db.Model):
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    comments = db.relationship('Comment', backref='post', lazy=True)
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
@@ -36,3 +39,13 @@ class Category(db.Model):
 
     def __repr__(self):
         return f"Category('{self.name}')"
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Comment('{self.content}', '{self.date_posted}')"
