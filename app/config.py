93760913d182cb98dcf4a0e1c7dd6bae6ec82d51
Beytullah Soy <114873625+bytsyw@users.noreply.github.com>
2024-05-20 20:40:44 +0300

    veritabanı halloldu

--- app/config.py
+++ app/config.py
@@ -5,5 +5,5 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'defaultsecretkey')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+pymysql://root:12345678@localhost/defaultdb')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+pymysql://root:12345678@localhost/wufoonet')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
